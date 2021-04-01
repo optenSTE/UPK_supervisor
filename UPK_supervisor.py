3c102cc3eabc6549108d1f2ac307e1b7a17a4482
optenSTE <38421767+optenSTE@users.noreply.github.com>
2021-04-01 15:30:52 +0300

    01.04.2021
    Стабильная версия, требуется тестирование

--- UPK_supervisor.py
+++ UPK_supervisor.py
@@ -3,6 +3,20 @@ UPK_supervisor
 Скрипт проверяет появление данных в указанной папке
 Если скорость поступления данных ниже порога, то выполняется перезапуск службы
 Также есть второй триггер, который перезапускает службу (без перезагрузки ИТО) с заданным интевалом
+
+Перезагрузка ИТО
+    если прибор не отвечает на тестовые команды или его IP:port недоступен,
+    то производится перезагрузка с помощью управляемой розетки NetPing 2/PWR-220 v12/ETH
+
+    Ограничения:
+    - осуществляется только при перезапуске службы
+    - ограничена частота перезагрузки
+    - если перезагрузка не привела к результату, то интервал между попытками увеличивается в два раза
+    - успешная перезагрузка возвращает интервал к первоначальному
+    - ограничено общее количество неуспешных перезагрузок
+
+
+
 ini-файл
 ;*********************************
 [main]
@@ -54,14 +68,26 @@ from pathlib import Path
 import socket
 import win32api
 import configparser
+from netpingrelay import NetpingRelay
 
-program_version = '14012021'
+program_version = '01.04.2021'
 
 # Глобальные переменные
 files_template = '*.txt'  # шаблон имени файла для подсчета размера папки
 instrument_description_filename = 'instrument_description.json'  # имя файла с описанием оборудования
 ITO_rebooting_duration_sec = 40  # время перезагрузки прибора
 win_service_restart_pause = 10  # пауза при перезапуске службы
+ito_ip = ''
+
+# Перекочует в ini
+netping_relay_address = '10.0.0.56'  # адрес управляемой розетки
+netping_relay_ito_socket_num = 2  # номер розетки, в которую воткнут ИТО
+ITO_reboot_min_interval_sec = 3600  # минимальный интервал между перезагрузками прибора (при отсутствии связи)
+ITO_reboot_max = 3  # максимальное количество перезагрузок прибора, сбрасывается при успешной связи с прибором
+
+ito_reboot_param = {'last_check_time': 0.0,
+                      'check_interval_desired': 3600, 'check_interval_actual': 3600, 'check_interval_multiplier': 2,
+                      'last_action_time': 0.0, 'actions_limit': 5,  'cur_failed_actions': 0}
 
 
 def get_dir_size_bytes(template):
@@ -72,7 +98,7 @@ def get_dir_size_bytes(template):
     return total_size
 
 
-def getFileProperties(fname):
+def get_file_properties(fname):
     """
     Read all properties of the given file return them as a dictionary.
     https://stackoverflow.com/questions/580924/how-to-access-a-files-properties-on-windows
@@ -102,7 +128,6 @@ def getFileProperties(fname):
         strInfo = {}
         for propName in propNames:
             strInfoPath = u'\\StringFileInfo\\%04X%04X\\%s' % (lang, codepage, propName)
-            ## print str_info
             strInfo[propName] = win32api.GetFileVersionInfo(fname, strInfoPath)
 
         props['StringFileInfo'] = strInfo
@@ -112,8 +137,47 @@ def getFileProperties(fname):
     return props
 
 
-def action_when_trigger_released(ITO_reboot=False):
+def ito_check_connection():
+    """
+    Проверка связи с ИТО: пинг порта прибора, затем тестовой командой из API
+    :return: True if ITO connected and answer commands
+            reason, str() - error description
+    """
 
+    ret = True
+    # проверка готовности прибора (должен отвечать порт, по которому идут команды)
+    with socket.socket() as s:
+        s.settimeout(1)
+        try:
+            s.connect((ito_ip, hyperion.COMMAND_PORT))  # подключаемся к порту команд
+        except socket.error:
+            ret = (1, f'command port is not active {ito_ip}:{hyperion.COMMAND_PORT}')
+        else:
+            # Hyperion command port test passed
+            # let's check how it responds to command
+            try:
+                h1 = hyperion.Hyperion(ito_ip)
+            except Exception as e:
+                ret = (2, f'Some error during ITO init - exception: {e.__doc__}')
+            else:
+                # ito connected, sending a command
+                try:
+                    h1.get_channel_detection_setting(1)
+                except Exception as e:
+                    ret = (3, f'Exception during h1.get_channel_detection_setting(1): {e.__doc__}')
+
+    return ret
+
+
+def action_when_trigger_released(ito_reboot=False):
+    """
+    Действия при срабатывании таймера, триггера
+    Перезапуск службы, перезапуск ИТО, установка часов ИТО
+
+    :param ito_reboot: boolean, ITO reboot permission
+    :return:
+    """
+    # stop the service
     try:
         logging.info(f'Stopping service {service_name}...')
         # stop the service
@@ -124,62 +188,81 @@ def action_when_trigger_released(ITO_reboot=False):
         logging.info(f"Pause for {win_service_restart_pause}sec")
         time.sleep(win_service_restart_pause)
     except Exception as e:
-        logging.error(f'An exception happened: {e.__doc__}')
+        logging.error(f'Exception during service stop: {e.__doc__}')
+        
+    # ITO check connection, reboot and clock set
+    if ito_reboot and (datetime.datetime.now().timestamp() - ito_reboot_param['last_check_time']) \
+            > ito_reboot_param['check_interval_actual']:
+        ito_reboot_param['last_check_time'] = datetime.datetime.now().timestamp()
 
-    # проверка готовности прибора (должен отвечать порт, по которому идут команды)
-    with socket.socket() as s:
-        s.settimeout(1)
-        instrument_address = (instrument_ip, hyperion.COMMAND_PORT)
-        try:
-            s.connect(instrument_address)  # подключаемся к порту команд
-        except socket.error:
-            logging.error(f'command port is not active {instrument_ip}:{hyperion.COMMAND_PORT}')
+        logging.info('Check ITO connection...')
+        ito_ok = ito_check_connection()
+        if ito_ok == True:
+            logging.info('Connection ok')
+
+            # интервал проверки можно вернуть на первоначальное значение
+            ito_reboot_param['check_interval_actual'] = ito_reboot_param['check_interval_desired']
+
+            # установка времени прибора
+            try:
+                h1 = hyperion.Hyperion(ito_ip)
+                logging.info(f'Current ITO time {h1.instrument_utc_date_time.strftime("%d.%m.%Y %H:%M:%S")}')
+
+                utcnow = datetime.datetime.utcnow()
+                logging.info(f'Setting ITO time to UPK-UTC {utcnow.strftime("%d.%m.%Y %H:%M:%S")}')
+                h1.instrument_utc_date_time = utcnow
+
+                logging.info(f'Current ITO time {h1.instrument_utc_date_time.strftime("%d.%m.%Y %H:%M:%S")}')
+            except Exception as e:
+                logging.debug(f'Some error during h1.instrument_utc_date_time - exception: {e.__doc__}')
+
         else:
-            logging.info(f"Hyperion command port test passed {instrument_ip}:{hyperion.COMMAND_PORT}")
+            logging.info(f'No connection: { ito_ok}')
 
-            if ITO_reboot:
-
+            # ITO doesn't answer - let's reboot it
+            if ito_reboot_param['cur_failed_actions'] < ito_reboot_param['actions_limit']:
                 try:
-                    h1 = hyperion.Hyperion(instrument_ip)
-                except Exception as e:
-                    logging.debug(f'Some error during ITO init - exception: {e.__doc__}')
-                else:
-
-                    try:
-                        logging.info(f'Current ITO time {h1.instrument_utc_date_time.strftime("%d.%m.%Y %H:%M:%S")}')
-
-                        utcnow = datetime.datetime.utcnow()
-                        logging.info(f'Setting ITO time to UPK-UTC {utcnow.strftime("%d.%m.%Y %H:%M:%S")}')
-                        h1.instrument_utc_date_time = utcnow
-
-                        logging.info(f'Current ITO time {h1.instrument_utc_date_time.strftime("%d.%m.%Y %H:%M:%S")}')
-
-                        logging.info(f"Pause for {win_service_restart_pause}sec")
-                        time.sleep(win_service_restart_pause)
-
-                    except Exception as e:
-                        logging.debug(f'Some error during h1.instrument_utc_date_time - exception: {e.__doc__}')
-
-                    try:
-                        logging.info(f'Rebooting ITO...')
-                        h1.reboot()
-
+                    logging.info('Cheking Netping socket...')
+                    relay = NetpingRelay(netping_relay_address)
+                    relay_ok = relay.check_connection()
+                    if relay_ok == True:
+                        logging.info('Netping socket connected.')
+                        logging.info(f'Rebooting ITO by Netping socket...')
+                        relay.reset_socket(netping_relay_ito_socket_num, 20)
                         logging.info(f"Pause for {ITO_rebooting_duration_sec}sec")
-                        time.sleep(ITO_rebooting_duration_sec)
-                        logging.info(f"Instrument {h1.instrument_name}, ip {instrument_ip} rebooted")
+                    else:
+                        logging.error(f'Netping socket error: {relay_ok}')
 
-                    except Exception as e:
-                        logging.error(f'An exception happened: {e.__doc__}')
+                except Exception as e:
+                    logging.error(f'An exception happened: {e.__doc__}')
 
+                else:
+                    ito_reboot_param['cur_failed_actions'] += 1
+                    time.sleep(ITO_rebooting_duration_sec)
+
+                # после перезагрузки нужно проверить связь с прибором
+                logging.info('Check ITO connection...')
+                ito_ok = ito_check_connection()
+                if ito_ok:
+                    logging.info('Connection ok')
+
+                    # интервал проверки можно вернуть на первоначальное значение
+                    ito_reboot_param['check_interval_actual'] = ito_reboot_param['check_interval_desired']
+                else:
+                    # перезагрузка не помогла, интервал проверки нужно увеличить
+                    ito_reboot_param['check_interval_actual'] = ito_reboot_param['check_interval_desired'] * \
+                                                                ito_reboot_param['check_interval_multiplier']
+                    return False
+
+    # start the service
     try:
-        # start the service
         logging.info(f'Starting service {service_name}...')
         args = ['sc', 'start', service_name]
         result2 = subprocess.run(args)
         logging.info(f'Start service {service_name} return code {result2.returncode}')
 
     except Exception as e:
-        logging.error(f'An exception happened: {e.__doc__}')
+        logging.error(f'Exception during service start: {e.__doc__}')
 
 
 if __name__ == "__main__":
@@ -221,7 +304,7 @@ if __name__ == "__main__":
         print(f'Error during ini-file reading: {str(e)}')
         sys.exit(0)
 
-    # check if folder exist, create if not
+    # check if data folder exist, create if not - for next log file
     try:
         os.makedirs(data_dir_path)
     except FileExistsError:
@@ -237,7 +320,7 @@ if __name__ == "__main__":
 
     logging.info(u'Program starts v.' + program_version)
     logging.info(f'EXE-file {sys.argv[0]}')
-    logging.info(getFileProperties(sys.argv[0]))
+    logging.info(get_file_properties(sys.argv[0]))
 
     # сохранить ini в логе
     logging.info(f'INI-file {ini_file_name}')
@@ -248,7 +331,6 @@ if __name__ == "__main__":
 
     last_dir_check_time = datetime.datetime.now().timestamp()
     last_unconditional_reboot_time = datetime.datetime.now().timestamp()
-    cur_time = 0
     cur_dir_size = 0
     last_dir_size = 0
     cur_num_of_triggers = 0
@@ -256,8 +338,8 @@ if __name__ == "__main__":
     num_of_service_restarts = 0
 
     logging.info(f'Looking for instrument description file {instrument_description_filename}...')
-    instrument_ip = None
-    while not instrument_ip:
+    ito_ip = None
+    while not ito_ip:
         # если есть задание на диске, то загрузим его и начнем работать до получения нового задания
         if Path(instrument_description_filename).is_file():
             try:
@@ -268,47 +350,46 @@ if __name__ == "__main__":
             else:
                 logging.info('Loaded instrument description ' + json.dumps(instrument_description))
 
-            instrument_ip = instrument_description['IP_address']
+            ito_ip = instrument_description['IP_address']
         else:
             logging.info(f'No file {instrument_description_filename}, pause for {dir_check_interval_sec} sec..')
             time.sleep(dir_check_interval_sec)
 
-    # print("cur_time\tlast_check_time\ttime_diff_sec\tdir_size_diff_byte\tcur_speed_mb_per_hour")
     while True:
+        time.sleep(1)
 
-        # безуслованя перезагрузка по таймеру
-        if win_service_restart_interval_sec > 0:
-            cur_time = datetime.datetime.now().timestamp()
-            if (cur_time - last_unconditional_reboot_time) >= win_service_restart_interval_sec > 0:
-
-                last_unconditional_reboot_time = cur_time
+        # Trigger2 - release periodically
+        try:
+            if (datetime.datetime.now().timestamp() - last_unconditional_reboot_time) >= win_service_restart_interval_sec > 0:
+                last_unconditional_reboot_time = datetime.datetime.now().timestamp()
 
                 logging.info('Trigger2 released')
                 action_when_trigger_released()
 
                 cur_num_of_triggers = 0
                 num_of_service_restarts = 0
+        except Exception as e:
+            logging.error(f'Trigger2 exception: {e.__doc__}')
 
-        # перезагрузка по скорости наполнения папки с данными
+        # Trigger1 - data folder surveillance
         try:
-            cur_time = datetime.datetime.now().timestamp()
-            cur_dir_size = get_dir_size_bytes(data_dir_path + '\\' + files_template)
-            if (cur_time - last_dir_check_time) >= dir_check_interval_sec:
+            # is the moment to check data folder?
+            if (datetime.datetime.now().timestamp() - last_dir_check_time) >= dir_check_interval_sec:
+                cur_dir_size = get_dir_size_bytes(data_dir_path + '\\' + files_template)
 
-                time_diff_sec = cur_time - last_dir_check_time
+                time_diff_sec = datetime.datetime.now().timestamp() - last_dir_check_time
                 dir_size_diff_byte = cur_dir_size - last_dir_size
+
+                last_dir_check_time = datetime.datetime.now().timestamp()
+                last_dir_size = cur_dir_size
 
                 if time_diff_sec <= 0:
                     raise NameError('time_diff_sec should be more than zero')
 
                 cur_speed_mb_per_h = 3600 / (1024 * 1024) * dir_size_diff_byte / time_diff_sec
-
-                # print(cur_time, last_dir_check_time, time_diff_sec, dir_size_diff_byte, cur_speed_mb_per_h, sep='\t')
-
                 logging.info('Speed, [Mb/h]\t%.3f' % cur_speed_mb_per_h)
-                if 0.0 <= cur_speed_mb_per_h < dir_size_speed_threshold_mb_per_h:
-                    # print('Speed %.1fMb/h is too slow' % cur_speed_mb_per_h)
 
+                if 0.0 <= cur_speed_mb_per_h < dir_size_speed_threshold_mb_per_h:
                     cur_num_of_triggers += 1
                     if cur_num_of_triggers >= num_of_triggers_before_action:
                         cur_num_of_triggers = 0
@@ -318,18 +399,9 @@ if __name__ == "__main__":
                             num_of_service_restarts = 0
                             ITO_reboot_now = True
 
-                        logging.info('Trigger1 released')
+                        logging.info(f'Trigger1 released, reboot_ITO={ITO_reboot_now}')
                         action_when_trigger_released(ITO_reboot_now)
                         num_of_service_restarts += 1
 
-            sleeping_time = dir_check_interval_sec - (cur_time - last_dir_check_time)
-            if sleeping_time < 0 or sleeping_time > dir_check_interval_sec:
-                sleeping_time = dir_check_interval_sec
-            time.sleep(sleeping_time)
-
         except Exception as e:
-            logging.error(f'An exception happened: {e.__doc__}')
-
-        finally:
-            last_dir_check_time = cur_time
-            last_dir_size = cur_dir_size
+            logging.error(f'Trigger1 exception: {e.__doc__}')
